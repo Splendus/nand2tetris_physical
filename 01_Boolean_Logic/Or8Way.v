c21e56990d1f5e4069017d1152b31fd5a8ea4fb9
Splendus <gengdominik@aol.com>
2025-07-07 10:02:56 +0200

    implement Or8way

--- 01_Boolean_Logic/Or8Way.v
+++ 01_Boolean_Logic/Or8Way.v
@@ -9,6 +9,24 @@ module Or8Way(
 	output out
 );
 
-	// Put your code here:
+    // using verilog built-in operators this could also be achieved with
+    // assign out = |in
+    // which sets `out` to 1 for any 1 in `in`
+    
+    // balanced or tree implementation:
+    wire [3:0] o2;
+    wire [1:0] o4;
+
+
+    Or OR20(.out(o2[0]),.a(in[0]),.b(in[1]));
+    Or OR21(.out(o2[1]),.a(in[2]),.b(in[3]));
+    Or OR22(.out(o2[2]),.a(in[4]),.b(in[5]));
+    Or OR23(.out(o2[3]),.a(in[6]),.b(in[7]));
+
+    Or OR40(.out(o4[0]),.a(o2[0]),.b(o2[1]));
+    Or OR41(.out(o4[1]),.a(o2[2]),.b(o2[3]));
+
+    Or ORFINAL(.out(out),.a(o4[0]),.b(o4[1]));
 
 endmodule
+
