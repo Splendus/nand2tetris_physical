c65bff164a1d68ee5e6c75f52e87e75a2cdd4bb7
Splendus <gengdominik@aol.com>
2025-07-09 15:53:36 +0200

    implement PC

--- 03_Sequential_Logic/PC.v
+++ 03_Sequential_Logic/PC.v
@@ -15,7 +15,27 @@ module PC(
 	input reset,
 	output [15:0] out
 );	
-	
-	// Put your code here:
+    
+    wire [15:0] inc_out;
+    wire [15:0] out_plus1;
+
+    wire [15:0] load_out;
+
+    wire [15:0] res_out;
+
+    wire or_inc_load;
+    wire or_all;
+
+    Inc16 out_inc (.out(out_plus1),.in(out));
+
+	Mux16 inc_mux (.out(inc_out),.a(out),.b(out_plus1),.sel(inc));
+	Mux16 load_mux (.out(load_out),.a(inc_out),.b(in),.sel(load));
+	Mux16 reset_mux (.out(res_out),.a(load_out),.b(16'b0),.sel(reset));
+
+    Or or1 (.out(or_inc_load),.a(inc),.b(load));
+    Or or2 (.out(or_all),.a(reset),.b(or_inc_load));
+
+    Register pc_out (.out(out),.in(res_out),.clk(clk),.load(or_all));
 
 endmodule
+
