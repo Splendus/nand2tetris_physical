6d107984033382cebabe4aabfa9cb2bf5edd3fe9
Splendus <gengdominik@aol.com>
2025-07-11 11:04:09 +0200

    implement blinky

--- 03_Sequential_Logic/08_blinky/blinky_tb.v
+++ 03_Sequential_Logic/08_blinky/blinky_tb.v
@@ -1,13 +1,17 @@
+`define SIM
+`timescale 1ns/1ns
 `default_nettype none
 module blinky_tb();
 
 	// IN,OUT
 	reg CLK=1;
+    reg[1:0] BUT;
 	wire[1:0] LED;
 
 	// Part
 	blinky blinky(
 		.CLK(CLK),
+        .BUT(BUT),
 		.LED(LED)
 	);
 	
@@ -17,7 +21,14 @@ module blinky_tb();
   	initial begin
   		$dumpfile("build/blinky_tb.vcd");
   		$dumpvars(0, blinky_tb);
-		#100000
+        // resets are inverted in blinky as buttons drive low
+        BUT[0] = 0;     // pre_reset
+        BUT[1] = 0;     // reset
+        #20;
+        BUT[0] = 1;
+        #20;
+        BUT[1] = 1;
+		#100000;
 		$finish;
 	end
 
